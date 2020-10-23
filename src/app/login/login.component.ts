84f881e9cbd12f6da36fbba9fa29fcbdf9aac6e9
victory chrisnanto <vkrisna65@gmail.com>
2020-10-23 10:47:06 +0700

    catch error login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,43 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { ERuangRapatService } from '../e-ruang-rapat.service';
-import {Router} from "@angular/router"
+import { Component, OnInit } from "@angular/core";
+import { ERuangRapatService } from "../e-ruang-rapat.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
+  username: string;
+  password: string;
+  loginError: boolean;
 
-  constructor(public us:ERuangRapatService, private router: Router) { }
-
-  username:string;
-  password:string;
+  constructor(public us: ERuangRapatService, private router: Router) {
+    this.username = "";
+    this.password = "";
+    this.loginError = false;
+  }
 
   ngOnInit() {
-    if(localStorage.getItem('access_token'))
-    {
-      this.router.navigate(['/home'])
+    if (localStorage.getItem("access_token")) {
+      this.router.navigate(["/home"]);
     }
   }
-  Authentication()
-  {
-    this.us.login(this.username,this.password).subscribe((data) => {
-      if(data['token'] != false)
-      {
-        window.location.reload()
-        this.router.navigate(['/home'])
-      }
-      else
-      {
-        console.log("PASSWORD SALAH");
+  Authentication() {
+    this.us.login(this.username, this.password).subscribe((data) => {
+      if (data["token"] != false) {
+        window.location.reload();
+        this.router.navigate(["/home"]);
+      } else {
+        this.loginError = true;
       }
     });
   }
-
-  token()
-  {
-    console.log(localStorage.getItem("access_token"));
+  onClose() {
+    this.loginError = false;
   }
 
+  token() {
+    console.log(localStorage.getItem("access_token"));
+  }
 }
