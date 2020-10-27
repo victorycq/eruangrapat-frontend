f4a15f31f283c190b949022ac41263f7ea53baf9
victory chrisnanto <vkrisna65@gmail.com>
2020-10-27 22:06:39 +0700

    define data type role_id

--- src/app/skpd/skpd.component.ts
+++ src/app/skpd/skpd.component.ts
@@ -1,44 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { MasterDataService } from '../services/masterData/master-data.service';
-import {Router} from "@angular/router";
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { MasterDataService } from "../services/masterData/master-data.service";
+import { Router } from "@angular/router";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-skpd',
-  templateUrl: './skpd.component.html',
-  styleUrls: ['./skpd.component.scss']
+  selector: "app-skpd",
+  templateUrl: "./skpd.component.html",
+  styleUrls: ["./skpd.component.scss"],
 })
 export class SkpdComponent implements OnInit {
-
-  constructor(public us:MasterDataService, private router: Router, public route: ActivatedRoute) { }
+  constructor(
+    public us: MasterDataService,
+    private router: Router,
+    public route: ActivatedRoute
+  ) {}
   placeholders = [];
   pageSize = 10;
   pageToLoadNext = 1;
   loading = false;
-  roleId= localStorage.getItem('role_id');
-  skpd:Array<any> = [{}] as any;
+  roleId: any = localStorage.getItem("role_id");
+  skpd: Array<any> = [{}] as any;
   ngOnInit() {
     this.loadNext();
   }
   loadNext() {
-    if (this.loading) { return }
+    if (this.loading) {
+      return;
+    }
 
     this.loading = true;
     this.placeholders = new Array(this.pageSize);
     this.placeholders = [];
     this.us.getPageSKPD(this.pageToLoadNext, this.pageSize).subscribe(
-          (data) => {
-            console.log(data);
-            this.placeholders = [];
-            this.skpd.push(...data['SKPD']);
-            this.loading = false;
-            this.pageToLoadNext++;
-            console.log(this.skpd);
-          },
-          (error) => {
-            console.log("message : " + error);
-          }
-        )
+      (data) => {
+        console.log(data);
+        this.placeholders = [];
+        this.skpd.push(...data["SKPD"]);
+        this.loading = false;
+        this.pageToLoadNext++;
+        console.log(this.skpd);
+      },
+      (error) => {
+        console.log("message : " + error);
+      }
+    );
   }
-
 }
